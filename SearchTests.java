8d6720148997980f2ba0f3f20ab6a33a223736e1
mmironov73 <insolent73@gmail.com>
2025-04-12 01:43:10 +0300

    hw 1 - done!

---
+++ SearchTests.java
@@ -0,0 +1,8 @@
+
+public class SearchTests {
+    @Test
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://selenide.org"));
+    }
